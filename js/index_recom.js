c8259656da16605e69b5de4d415719422ac61744
yzh <710691246@qq.com>
2019-09-27 19:09:21 +0800

    完善轮播图

--- js/index_recom.js
+++ js/index_recom.js
@@ -10,29 +10,30 @@ $(function() {
 				str += `
 						<li class="ph">	
 							<a href=''>
-								<img src = '${data.data.list[i].image}'>
+								<img src = '${data.data.list[i].image}' class="banpic">
 							</a>
 						</li>
 						`;
 			}
 			$(".pic").append(str);
 			//轮播
+
 			
+
+			for(var j=0;j<$(".banpic").length;j++){
+				$(".banpic").eq(j).css({"left":j*$('.banpic').width()+'px'});
+			}
+			$(".ph").eq(index).animate({left:"0px"},1000);
+		
 			setInterval(function(){
 				index++;
 				if(index>$(".ph").length-1){index = 0;}
-				
-				$(".ph").eq(index).fadeIn().siblings().fadeOut();
-				$(".circle li").eq(index).animate({width:"0.25rem"}).siblings().animate({width:"0.1rem"});
-				
+				console.log(index);
+				$(".ph").eq(index).animate({left:"0px"},1000);
+				$(".ph").eq(index-1).animate({left:"-3.5rem"},1000)
+				.siblings().css({"left":"3.5rem"});
+				$(".circle li").eq(index).animate({width:"0.25rem"},1000).siblings().animate({width:"0.1rem"},1000);
 			},3500)
-			$(".circle li").on("click", function(){
-				$(this).animate({width:"0.25rem"}).siblings().animate({width:"0.1rem"});
-				$(".ph").eq($(this).index()).fadeIn().siblings().fadeOut();
-				index = $(this).index();
-			});
-			
-			
 		}
 	});
 	
